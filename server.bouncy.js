5651fa7e2d0e57ea39301cd602f24d6f1050bfd5
Touno.K <info.dvgamer@gmail.com>
2015-10-19 20:53:28 +0700

    update package

--- server.bouncy.js
+++ server.bouncy.js
@@ -1,16 +1,46 @@
-var bouncy = require('bouncy');
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var bouncy = require('bouncy'), ini = require('iniparser'), Q = require('q');
+
+var url = 'mongodb://192.168.10.217:27017/domain';
 
 var server = bouncy(function (req, res, bounce) {
-    if (req.headers.host === 'beep.example.com') {
-        bounce(8001);
-    }
-    else if (req.headers.host === 'boop.example.com') {
-        bounce(8002);
-    }
-    else {
-        res.statusCode = 404;
-        res.end('no such host');
-    }
+    // var deferred = Q.defer();
+
+    // Q.fcall(promisedStep1)
+    // .then(promisedStep2)
+    // .then(promisedStep3)
+    // .then(promisedStep4)
+    // .then(function (value4) {
+    //     // Do something with value4
+    // })
+    // .catch(function (error) {
+    //     // Handle any error from all above steps
+    // })
+    // .done();
+
+    // FS.readFile("foo.txt", "utf-8", function (error, text) {
+    //     if (error) {
+    //         deferred.reject(new Error(error));
+    //     } else {
+    //         deferred.resolve(text);
+    //     }
+    // });
+    // return deferred.promise;
+
+    // ini.parse('./config.ini', function(err,data){
+        
+    // });
+    
+
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected", req.headers.host,"correctly to server.");
+      db.close();
+
+      bounce(8002);
+    });
+
+
 });
 
 server.listen(80);
